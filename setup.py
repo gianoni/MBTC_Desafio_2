e5e0a097b8499dbd8cd893d32c6e11df1b3153e6
Thiago Lopes <68572112+talopes78@users.noreply.github.com>
2020-08-22 15:04:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,5 @@ setup(
       packages=[
             'my_custom_sklearn_transforms'
       ],
-      zip_safe=False,
-      install_requires=[
-            'imbalanced-learn==0.4.2',
-      ],
+
 )
